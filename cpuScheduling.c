88a24c17d573f8908b8fc333815d05380b6484bb
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2024-07-23 12:34:20 +0530

    Menu Driven

--- cpuScheduling.c
+++ cpuScheduling.c
@@ -4,7 +4,7 @@
 void main(){
  int choice;
  while(true){
-    printf("Enter the choice\n 1)FCFS 2)SJF 3)Priority 4)Exit \n");
+    printf("Enter the choice\n 1)FCFS 2)SJF 3)Priority 4)Round Robin 5)Exit \n");
     scanf("%d",&choice);
     switch(choice){
     		case 1:{
@@ -131,11 +131,57 @@ void main(){
 			     printf("%d\t%d\t%d\t%d\t%d\n",process[i],bt[i],priority[i],wt[i],tat[i]);
 			printf("Total WT = %.2f , Avg WT = %.2f\nTotal TAT = %.2f , Avg TAT = %.2f\n",tot_wt,tot_wt/limit,tot_tat,tot_tat/limit);
 			break;
-                 }
-                 case 4: exit(0);
-                 default : printf("Invalid choice");
-                 
-       }
-    }
-   }
+                      }
+                 case 4:{
+                 	printf("Enter time quantum\n");
+   		        int TIME_QUANTUM;
+    			scanf("%d", &TIME_QUANTUM);
+			printf("Enter number of processes\n");
+   		        int limit;
+   		        scanf("%d", &limit);
+   		        int process[limit], bt[limit], rem_bt[limit], wt[limit], tat[limit], i;
+		        int time = 0;
+			int completed = 0;
+                        printf("Enter processes and burst time\n");
+		        for (i = 0; i < limit; i++) {
+			  scanf("%d%d", &process[i], &bt[i]);
+			  rem_bt[i] = bt[i];
+		        }
+                        printf("\nGantt Chart:\n");
+		        printf("0");
+		        while (completed < limit) {
+			   for (i = 0; i < limit; i++) {
+			      if (rem_bt[i] > 0) {
+				if (rem_bt[i] > TIME_QUANTUM) {
+				    printf("  P%d ", process[i]);
+				    time += TIME_QUANTUM;
+				    rem_bt[i] -= TIME_QUANTUM;
+				} else {
+				    printf("  P%d ", process[i]);
+				    time += rem_bt[i];
+				    wt[i] = time - bt[i];
+				    rem_bt[i] = 0;
+				    completed++;
+				    tat[i] = time;
+				}
+				printf("%d", time);
+			      }
+			   }
+		      }
+		    printf(" \n");
+		    float tot_wt = 0, tot_tat = 0;
+		    printf("\nProcess\tBT\tWT\tTAT\n");
+		    for (i = 0; i < limit; i++) {
+			printf("%d\t%d\t%d\t%d\n", process[i], bt[i], wt[i], tat[i]);
+			tot_wt += wt[i];
+			tot_tat += tat[i];
+		    }
+		    printf("Total WT = %.2f , Avg WT = %.2f\nTotal TAT = %.2f , Avg TAT = %.2f\n",tot_wt,tot_wt/limit,tot_tat,tot_tat/limit);
+		    break;
+		   }
+	      case 5: exit(0);
+	      default : printf("Invalid choice");	 
+              }
+	 }
+      }
     
