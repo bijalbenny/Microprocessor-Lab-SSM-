b46562c81d96ced9f70811c2ab25e1b34f193ec1
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2024-11-04 23:53:06 +0530

    Create absoluteLoader.c

---
+++ Absolute_Loader/absoluteLoader.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void main() {
+    FILE *fp;
+    int i, j, length;
+    char line[50], arr[10];
+    fp = fopen("record.txt", "r");
+    fscanf(fp, "%s", line);
+
+    printf("The Program name is : ");
+    for(int i=4; i<8; i++) {
+        printf("%c", line[i]);
+    }
+    printf("\n");
+
+    do {
+        fscanf(fp, "%s", line);
+        if(line[0] == 'T') {
+            for(i=0; i<6; i++) {
+                arr[i] = line[i+2];
+            }
+            arr[i] = '\0';
+            length = atoi(arr);
+            
+            i=12;
+            while(line[i] != '$') {
+                if(line[i] != '^') {
+                    printf("%d : \t %c%c\n", length, line[i], line[i+1]);
+				    length++;
+				    i = i+2;
+                } else {
+                    i++;
+                }
+            }
+        } else if(line[0] == 'E') {
+            break;
+        }
+    } while(!feof(fp));
+
+    fclose(fp);
+
+}
